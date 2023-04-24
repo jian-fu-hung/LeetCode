bde546efe43aa2a9712ec69f4054487dbfa1da95
jian-fu-hung <jerryhome555@gmail.com>
2023-04-24 23:38:47 +0800

    1046. Last Stone Weight

--- src/main/kotlin/LastStoneWeight/LastStoneWeight.kt
+++ src/main/kotlin/LastStoneWeight/LastStoneWeight.kt
@@ -1,35 +1,36 @@
 package LastStoneWeight
 
+import java.util.*
+import kotlin.Comparator
+
 class Solution {
-
-
-
+    fun runTest() {
+        lastStoneWeight(intArrayOf(2,7,4,1,8,1))
+    }
     fun lastStoneWeight(stones: IntArray): Int {
-
-        var array = stones
-        array.sort()
-        while (array.size > 1){
-            array = destroyed(array)
-            array.sort()
+        var queue = PriorityQueue<Int>(Collections.reverseOrder())
+        stones.forEach {
+            queue.offer(it)
         }
-        if(array.size <= 0 ){
+
+        while (queue.size > 1) {
+            val one: Int = queue.poll()
+            val two: Int = queue.poll()
+            when {
+                one > two -> {
+                    val newItem = one - two
+                    queue.offer(newItem)
+                }
+                one < two -> {
+                    val newItem = two - one
+                    queue.offer(newItem)
+                }
+            }
+        }
+        if (queue.size == 1) {
+            return queue.peek()
+        } else {
             return 0
         }
-        return array[0]
-
     }
-
-    fun destroyed(array: IntArray): IntArray{
-        val a = array.size-1
-        val b = a - 1
-        val mutableMap = array.toMutableList()
-        mutableMap.removeAt(a)
-        mutableMap.removeAt(b)
-        if(array[a] - array[b] != 0){
-            mutableMap.add(array[a] - array[b])
-        }
-        return mutableMap.toIntArray()
-    }
-
-
 }
